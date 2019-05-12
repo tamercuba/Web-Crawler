22d4fa4af3c6682bc56abe1684f0ae2a7e8a08c1
tamercuba <tamercuba@gmail.com>
2019-05-12 17:37:25 -0300

    processos mais organizados

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 import scrapy
 from app.spiders.vultr import VultrSpider
 from scrapy.crawler import CrawlerProcess
-from scrapy.settings import Settings
 from scrapy.utils.project import get_project_settings
 
-process = CrawlerProcess(get_project_settings())
+class Main():
+    def __init__(self):
+        settings = get_project_settings()
+        process = CrawlerProcess(settings)
+        process.crawl(VultrSpider())
+        process.start()
 
-process.crawl(VultrSpider())
-
-process.start()
+if __name__ == '__main__':
+    main = Main()
