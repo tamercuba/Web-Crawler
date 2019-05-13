00f80e3d63e345b64c8dfaf0363e5b1b0eb9f333
tamercuba <tamercuba@gmail.com>
2019-05-13 12:00:47 -0300

    pipeline csv adicionada

--- app/pipelines.py
+++ app/pipelines.py
@@ -1,20 +1,33 @@
 import json
-from scrapy.exporters import JsonItemExporter
+from scrapy.exporters import JsonItemExporter, CsvItemExporter
 
 class CustomJsonExporter(JsonItemExporter):
     def _beautify_newline(self):
         self.file.write(b'\n')
 
-class PrintItem(object):
+class PrintItem:
     def process_item(self, item, spider):
         print(dict(item))
         return item
 
-class JsonPipeline(object):
+class JsonPipeline:
     def open_spider(self, spider):
         self.file       = open('static/maquinas.json', 'wb')
         self.exporter   = CustomJsonExporter(self.file)
         self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+class CsvPipeline:
+    def open_spider(self, spider):
+        self.file       = open('static/maquinas.csv', 'wb')
+        self.exporter   = CsvItemExporter(self.file)
 
     def close_spider(self, spider):
         self.exporter.finish_exporting()
