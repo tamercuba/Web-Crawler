1fbd4088713e96705e9a48e5534fbf375d8d3460
tamercuba <tamercuba@gmail.com>
2019-05-14 11:40:16 -0300

    versao digital ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import scrapy
-from app.spiders.vultr import VultrSpider
+from app.spiders.spider_maquinas import VultrSpider, DigitalOceanSpider
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
@@ -8,7 +8,8 @@ class Main():
     def __init__(self):
         settings = get_project_settings()
         process = CrawlerProcess(settings)
-        process.crawl(VultrSpider())
+        #process.crawl(VultrSpider())
+        process.crawl(DigitalOceanSpider())
         process.start()
 
 if __name__ == '__main__':
