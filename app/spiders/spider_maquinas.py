9bd917092bec79e2738c2abed9e162099f36dcbf
tamercuba <tamercuba@gmail.com>
2019-05-16 11:02:30 -0300

    print spider adicionado

--- app/spiders/spider_maquinas.py
+++ app/spiders/spider_maquinas.py
@@ -3,10 +3,14 @@ from ..items import ProductItem
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import MapCompose, Join
 
+def print_name(spider):
+	print(spider.name, ':')
+
 class DigitalOceanSpider(scrapy.Spider):
 	name = "digital_ocean"
 	start_urls = ['https://www.digitalocean.com/pricing/']
 	def parse(self, response):
+		print_name(self)
 		product = ProductItem()
 		ALL_ROWS = response.xpath('//div[@id="standard-droplets-pricing-table"]').css('table.PricingTable').css('tbody').css('tr')
 		for row in ALL_ROWS:
@@ -16,31 +20,13 @@ class DigitalOceanSpider(scrapy.Spider):
 			product['bandwidth'] = row.css('td::text')[4].extract().strip()
 			product['price'] = row.css('td')[4].css('strong::text').extract_first()
 			yield product
-#
-# class DigitalOceanSpider(scrapy.Spider):
-# 	name = "digital_ocean"
-# 	start_urls = ['https://www.digitalocean.com/pricing/']
-#
-# 	def parse(self, response):
-# 		product = ItemLoader( ProductItem(), response=response)
-# 		product.default_input_processor = MapCompose(lambda v: v.split, replace_escape_chars)
-# 		product.default_input_processor = Join()
-# 		ALL_ROWS = response.xpath('//div[@id="standard-droplets-pricing-table"]').css('table.PricingTable').css('tbody').css('tr')
-# 		for row in ALL_ROWS:
-# 			items = []
-# 			product.add_css('storage',row.css('td::text')[2])
-# 			product.add_css('cpu', row.css('td::text')[1])
-# 			product.add_css('memory',row.css('td')[0].css('strong::text'))
-# 			product.add_css('bandwidth',row.css('td::text')[3])
-# 			product.add_css('price',row.css('td')[4].css('strong::text'))
-# 			items.append(product.load_item())
-# 			yield items
 
 class VultrSpider(scrapy.Spider):
 	name 		= "vultr_spider"
 	start_urls 	= ['https://www.vultr.com/pricing/']
 
 	def parse(self, response):
+		print_name(self)
 		product = ProductItem()
 		ALL_ROWS = response.css('div.pt__row').css('div.pt__row-content')
 		for row in ALL_ROWS:
